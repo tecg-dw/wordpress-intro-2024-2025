5c982d46564b0c5300e3c66c6aa1313045c16e45
Toon Van den Bos <toon@whitecube.be>
2025-02-07 16:02:14 +0100

    Initial commit (w202)

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
+    <title>Document</title>
 </head>
 <body>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
+    <h1>Salut!</h1>
 </body>
 </html>
