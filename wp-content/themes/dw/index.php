23bb411b601df1302d42c644b971945d015a41bb
Toon Van den Bos <toon@whitecube.be>
2025-02-14 14:19:14 +0100

    Added first iteration example of "The Loop"

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -6,7 +6,26 @@
     <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
 </head>
 <body>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
+    <header>
+        <h1><?= get_bloginfo('name') ?></h1>
+        <p><?= get_bloginfo('description') ?></p>
+    </header>
+
+    <main>
+        <?php 
+        // On ouvre "la boucle" (The Loop), la structure de contrôle
+        // de contenu propre à Wordpress:
+        if(have_posts()): while(have_posts()): the_post(); ?>
+
+            <h2><?= get_the_title(); ?></h2>
+
+            <div><?= get_the_content(); ?></div>
+
+        <?php 
+        // On ferme "la boucle" (The Loop):
+        endwhile; else: ?>
+            <p>Pas de contenu à afficher.</p>
+        <?php endif; ?>
+    </main>
 </body>
 </html>
