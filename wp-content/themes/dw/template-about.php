4002cd2612e63cf6d2a31fa1126fba6ae1129b89
Toon Van den Bos <toon@whitecube.be>
2025-02-14 14:51:09 +0100

    Added custom static page template

---
+++ wp-content/themes/dw/template-about.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Template "À propos" */ ?>
+<?php get_header(); ?>
+    <h2>À propos de moi</h2>
+    <?php 
+    // On ouvre "la boucle" (The Loop), la structure de contrôle
+    // de contenu propre à Wordpress:
+    if(have_posts()): while(have_posts()): the_post(); ?>
+
+        <div><?= get_the_content(); ?></div>
+
+    <?php 
+    // On ferme "la boucle" (The Loop):
+    endwhile; else: ?>
+        <p>Pas de contenu à afficher.</p>
+    <?php endif; ?>
+<?php get_footer(); ?>
