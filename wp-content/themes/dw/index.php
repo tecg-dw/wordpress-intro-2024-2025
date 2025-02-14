635d436cbdcf3930c7de281d88b34a8625ce14b0
Toon Van den Bos <toon@whitecube.be>
2025-02-14 14:41:00 +0100

    Added header & footer template files

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,31 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
-    <header>
-        <h1><?= get_bloginfo('name') ?></h1>
-        <p><?= get_bloginfo('description') ?></p>
-    </header>
+<?php get_header(); ?>
+    <?php 
+    // On ouvre "la boucle" (The Loop), la structure de contrôle
+    // de contenu propre à Wordpress:
+    if(have_posts()): while(have_posts()): the_post(); ?>
 
-    <main>
-        <?php 
-        // On ouvre "la boucle" (The Loop), la structure de contrôle
-        // de contenu propre à Wordpress:
-        if(have_posts()): while(have_posts()): the_post(); ?>
+        <h2><?= get_the_title(); ?></h2>
 
-            <h2><?= get_the_title(); ?></h2>
+        <div><?= get_the_content(); ?></div>
 
-            <div><?= get_the_content(); ?></div>
-
-        <?php 
-        // On ferme "la boucle" (The Loop):
-        endwhile; else: ?>
-            <p>Pas de contenu à afficher.</p>
-        <?php endif; ?>
-    </main>
-</body>
-</html>
+    <?php 
+    // On ferme "la boucle" (The Loop):
+    endwhile; else: ?>
+        <p>Pas de contenu à afficher.</p>
+    <?php endif; ?>
+<?php get_footer(); ?>
